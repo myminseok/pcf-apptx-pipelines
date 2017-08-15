1d390aa63bff11f953481e20e7d7d8f5a1f14c03
Mark Alston <malston@pivotal.io>
2017-08-15 17:34:08 -0600

    Use functions from functions folder instead local task folder

--- tasks/build-and-upload/task.sh
+++ tasks/build-and-upload/task.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export ROOT_FOLDER=$( pwd )
 export REPO_RESOURCE=repo
@@ -10,11 +10,15 @@ export TOOLS_RESOURCE=tools
 export VERSION_RESOURCE=version
 export OUTPUT_RESOURCE=out
 
+FUNCTIONS_FOLDER=${ROOT_FOLDER}/${TOOLS_RESOURCE}/functions
+TASKS_FOLDER=${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks
+
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
 echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
-echo "Current working directory is [${cwd}]"
+echo "Functions folder is [${FUNCTIONS_FOLDER}]"
+echo "Tasks folder is [${TASKS_FOLDER}]"
 
 echo "Retrieving version"
 export PIPELINE_VERSION=$( cat ${ROOT_FOLDER}/${VERSION_RESOURCE}/version )
@@ -23,26 +27,20 @@ echo "Retrieved version is [${PIPELINE_VERSION}]"
 export CI="CONCOURSE"
 export ENVIRONMENT=BUILD
 
-source ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/common.sh
+source ${TASKS_FOLDER}/common.sh
 
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
 # CURRENTLY WE ONLY SUPPORT JVM BASED PROJECTS OUT OF THE BOX
-[[ -f "${cwd}/projectType/pipeline-jvm.sh" ]] && source "${cwd}/projectType/pipeline-jvm.sh" || \
-    echo "No ${cwd}/projectType/pipeline-jvm.sh found"
-
-export OUTPUT_FOLDER=$( outputFolder )
-export TEST_REPORTS_FOLDER=$( testResultsAntPattern )
-
-echo "Output folder [${OUTPUT_FOLDER}]"
+[[ -f "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" ]] && source "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" || \
+    echo "No ${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh found"
 
 echo "Generating settings.xml / gradle properties for Maven in local m2"
-source ${cwd}/generate-settings.sh
+source ${CWD}/generate-settings.sh
 
 export TERM=dumb
 
 echo "Building and uploading the projects artifacts"
-cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
 build
 
