c620e8cd1ae12e0e23825e9fdf889e223924fec4
Mark Alston <malston@pivotal.io>
2017-08-16 08:55:28 -0600

    Refactor test-rollback-smoke like the test-smoke task

--- tasks/test-rollback-smoke/pipeline.sh
+++ tasks/test-rollback-smoke/pipeline.sh
@@ -1,72 +1,8 @@
 #!/bin/bash
 set -e
 
-function logInToPaas() {
-    local user="PAAS_${ENVIRONMENT}_USERNAME"
-    local cfUsername="${!user}"
-    local pass="PAAS_${ENVIRONMENT}_PASSWORD"
-    local cfPassword="${!pass}"
-    local org="PAAS_${ENVIRONMENT}_ORG"
-    local cfOrg="${!org}"
-    local space="PAAS_${ENVIRONMENT}_SPACE"
-    local cfSpace="${!space}"
-    local api="PAAS_${ENVIRONMENT}_API_URL"
-    local apiUrl="${!api:-api.run.pivotal.io}"
-    CF_INSTALLED="$( cf --version || echo "false" )"
-    CF_DOWNLOADED="$( test -r cf && echo "true" || echo "false" )"
-    echo "CF Installed? [${CF_INSTALLED}], CF Downloaded? [${CF_DOWNLOADED}]"
-    if [[ ${CF_INSTALLED} == "false" && ${CF_DOWNLOADED} == "false" ]]; then
-        echo "Downloading Cloud Foundry"
-        curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" --fail | tar -zx
-        CF_DOWNLOADED="true"
-    else
-        echo "CF is already installed"
-    fi
-
-    if [[ ${CF_DOWNLOADED} == "true" ]]; then
-        echo "Adding CF to PATH"
-        PATH=${PATH}:`pwd`
-        chmod +x cf
-    fi
-
-    echo "Cloud foundry version"
-    cf --version
-
-    echo "Logging in to CF to org [${cfOrg}], space [${cfSpace}]"
-    cf api --skip-ssl-validation "${apiUrl}"
-    cf login -u "${cfUsername}" -p "${cfPassword}" -o "${cfOrg}" -s "${cfSpace}"
-}
-
-function appHost() {
-    local appName="${1}"
-    local lowerCase="$( toLowerCase "${appName}" )"
-    local APP_HOST=`cf apps | awk -v "app=${lowerCase}" '$1 == app {print($0)}' | tr -s ' ' | cut -d' ' -f 6 | cut -d, -f1`
-    echo "${APP_HOST}" | tail -1
-}
-
-function propagatePropertiesForTests() {
-    local projectArtifactId="${1}"
-    local stubRunnerHost="${2:-stubrunner-${projectArtifactId}}"
-    local fileLocation="${3:-${OUTPUT_FOLDER}/test.properties}"
-    echo "Propagating properties for tests. Project [${projectArtifactId}] stub runner host [${stubRunnerHost}] properties location [${fileLocation}]"
-    # retrieve host of the app / stubrunner
-    # we have to store them in a file that will be picked as properties
-    rm -rf "${fileLocation}"
-    local host=$( appHost "${projectArtifactId}" )
-    export APPLICATION_URL="${host}"
-    echo "APPLICATION_URL=${host}" >> ${fileLocation}
-    host=$( appHost "${stubRunnerHost}" )
-    export STUBRUNNER_URL="${host}"
-    echo "STUBRUNNER_URL=${host}" >> ${fileLocation}
-    echo "Resolved properties"
-    cat ${fileLocation}
-}
-
-function toLowerCase() {
-    local string=${1}
-    local result=$( echo "${string}" | tr '[:upper:]' '[:lower:]' )
-    echo "${result}"
-}
+[[ -f "${FUNCTIONS_FOLDER}/pipeline-cf.sh" ]] && source "${FUNCTIONS_FOLDER}/pipeline-cf.sh" || \
+    echo "No ${FUNCTIONS_FOLDER}/pipeline-cf.sh found"
 
 function prepareForSmokeTests() {
     echo "Retrieving group and artifact id - it can take a while..."
@@ -78,19 +14,4 @@ function prepareForSmokeTests() {
     echo "Application URL [${APPLICATION_URL}]"
     echo "StubRunner URL [${STUBRUNNER_URL}]"
     echo "Latest production tag [${LATEST_PROD_TAG}]"
-}
-
-function readTestPropertiesFromFile() {
-    local fileLocation="${1:-${OUTPUT_FOLDER}/test.properties}"
-    if [ -f "${fileLocation}" ]
-    then
-      echo "${fileLocation} found."
-      while IFS='=' read -r key value
-      do
-        key=$(echo ${key} | tr '.' '_')
-        eval "${key}='${value}'"
-      done < "${fileLocation}"
-    else
-      echo "${fileLocation} not found."
-    fi
 }
--- tasks/test-rollback-smoke/projectType/pipeline-gradle.sh
+++ tasks/test-rollback-smoke/projectType/pipeline-gradle.sh
@@ -13,24 +13,4 @@ function runSmokeTests() {
     fi
 }
 
-function retrieveAppName() {
-    local result=$( ./gradlew artifactId -q )
-    result=$( echo "${result}" | tail -1 )
-    echo "${result}"
-}
-
-function printTestResults() {
-    echo -e "\n\nBuild failed!!! - will print all test results to the console (it's the easiest way to debug anything later)\n\n" && tail -n +1 "$( testResultsAntPattern )"
-}
-
-function outputFolder() {
-    echo "build/libs"
-}
-
-function testResultsAntPattern() {
-    echo "**/test-results/*.xml"
-}
-
 export -f runSmokeTests
-export -f outputFolder
-export -f testResultsAntPattern
