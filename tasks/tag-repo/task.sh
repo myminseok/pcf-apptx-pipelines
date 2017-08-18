f5e11fb21117043ee3f08ae86ff384993f576354
Mark Alston <malston@pivotal.io>
2017-08-18 15:26:58 -0600

    Add optional APPEND_TIMESTAMP param

--- tasks/tag-repo/task.sh
+++ tasks/tag-repo/task.sh
@@ -16,6 +16,11 @@ echo "Version resource folder is [${VERSION_RESOURCE}]"
 export PIPELINE_VERSION=$( cat ${ROOT_FOLDER}/${VERSION_RESOURCE}/version )
 echo "Retrieved version is [${PIPELINE_VERSION}]"
 
-echo "Tagging the project with ${TAG_PREFIX} tag"
-echo "${TAG_PREFIX}/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
+TAG_VERSION=$PIPELINE_VERSION
+if [[ ${APPEND_TIMESTAMP} == "true" ]]; then
+  TAG_VERSION=$TAG_VERSION-`date +%Y%m%d_%H%M%S`
+fi
+
+echo "Tagging the project with ${TAG_PREFIX}/${TAG_VERSION} tag"
+echo "${TAG_PREFIX}/${TAG_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
 cp -r ${ROOT_FOLDER}/${REPO_RESOURCE}/. ${ROOT_FOLDER}/${OUTPUT_RESOURCE}/
