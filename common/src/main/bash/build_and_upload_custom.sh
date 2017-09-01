24598cd1e9abac472d7d396852afd0af866969a2
Mark Alston <malston@pivotal.io>
2017-09-01 15:40:19 -0600

    Remove source of common.sh

--- common/src/main/bash/build_and_upload_custom.sh
+++ common/src/main/bash/build_and_upload_custom.sh
@@ -2,20 +2,16 @@
 
 set -o errexit
 
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 FUNCTIONS_FOLDER=${ROOT_FOLDER}/${APPTX_RESOURCE}/functions
 TASKS_FOLDER=${ROOT_FOLDER}/${APPTX_RESOURCE}/tasks
 
 echo "AppTx Functions folder is [${FUNCTIONS_FOLDER}]"
 echo "AppTx Tasks folder is [${TASKS_FOLDER}]"
 
-source ${TASKS_FOLDER}/common.sh
-
 # CURRENTLY WE ONLY SUPPORT JVM BASED PROJECTS OUT OF THE BOX
 [[ -f "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" ]] && source "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" || \
     echo "No ${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh found"
 
 lowerCaseProjectType=$( echo "${PROJECT_TYPE}" | tr '[:upper:]' '[:lower:]' )
-[[ -f "${CWD}/projectType/pipeline-${lowerCaseProjectType}.sh" ]] && source "${CWD}/projectType/pipeline-${lowerCaseProjectType}.sh" || \
-    echo "No ${CWD}/projectType/pipeline-${lowerCaseProjectType}.sh found"
+[[ -f "${TASKS_FOLDER}/build-and-upload/projectType/pipeline-${lowerCaseProjectType}.sh" ]] && source "${TASKS_FOLDER}/build-and-upload/projectType/pipeline-${lowerCaseProjectType}.sh" || \
+    echo "No ${TASKS_FOLDER}/build-and-upload/projectType/pipeline-${lowerCaseProjectType}.sh found"
