3d696df8b45a22d9e4840298bf56578ab8bdd18e
Mark Alston <malston@pivotal.io>
2017-09-01 13:40:21 -0600

    Reset __DIR

--- common/src/main/bash/build_and_upload.sh
+++ common/src/main/bash/build_and_upload.sh
@@ -9,11 +9,13 @@ export ENVIRONMENT=BUILD
 [[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" || \
     echo "No pipeline.sh found"
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 CUSTOM_SCRIPT="$(basename "${BASH_SOURCE}" | sed 's/.sh/_custom.sh/g')"
 
 echo "Custom script name is [${CUSTOM_SCRIPT}]"
 
 [[ -f "${__DIR}/${CUSTOM_SCRIPT}" ]] && source "${__DIR}/${CUSTOM_SCRIPT}" || \
-    echo "No ${CUSTOM_SCRIPT} found"
+    echo "No ${__DIR}/${CUSTOM_SCRIPT} found"
 
 build
