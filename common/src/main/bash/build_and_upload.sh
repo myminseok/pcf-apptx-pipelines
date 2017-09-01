4d24e48daa9aaca56f032c815548fb981adb4af9
Mark Alston <malston@pivotal.io>
2017-09-01 13:14:14 -0600

    Spiking

--- common/src/main/bash/build_and_upload.sh
+++ common/src/main/bash/build_and_upload.sh
@@ -9,7 +9,9 @@ export ENVIRONMENT=BUILD
 [[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" || \
     echo "No pipeline.sh found"
 
-__SCRIPT_NAME="$(dirname "${BASH_SOURCE[0]}")"
+__SCRIPT_NAME="$(basename "$0")"
+
+echo "Custom script name is [${__SCRIPT_NAME}]"
 
 [[ -f "${__DIR}/${__SCRIPT_NAME}_custom.sh" ]] && source "${__DIR}/${__SCRIPT_NAME}_custom.sh" || \
     echo "No ${__SCRIPT_NAME}_custom.sh found"
